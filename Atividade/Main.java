527a7692bc0816943d00ef8c452bbe46c61d82cd
caiocarames <119985436+caiocarames@users.noreply.github.com>
2025-03-10 18:28:58 -0300

    Update Main.java

--- Atividade/Main.java
+++ Atividade/Main.java
@@ -1,9 +1,7 @@
-
 public class Main {
-
     public static void main(String[] args) {
-        GerenciadorNomes gNomes = new GerenciadorNomesMem();
-        Ihm ihm = new Ihm(gNomes);
-        ihm.dialogar();
+        GerenciadorNomes gerenciador = new GerenciadorNomesBD(); 
+        Ihm ihm = new Ihm(gerenciador);
+        ihm.dialogar(); 
     }
 }
