8824cfe5e78465007c3113fca1629238cf65292a
caiocarames <119985436+caiocarames@users.noreply.github.com>
2025-03-10 18:23:07 -0300

    Update GerenciadorNomesBD.java

--- Atividade/GerenciadorNomesBD.java
+++ Atividade/GerenciadorNomesBD.java
@@ -3,23 +3,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GerenciadorNomesBD implements GerenciadorNomes {
-    private static final String URL = "jdbc:sqlite:names.db"; // Caminho do banco de dados
+    private static final String URL = "jdbc:postgresql://db.pxuzgxpcmtpcfescntjd.supabase.co:5432/postgres";
+    private static final String USER = "postgres"; 
+    private static final String PASSWORD = "atividade12345"; 
 
+    
     private static final String CREATE_TABLE_SQL = 
-        "CREATE TABLE IF NOT EXISTS nomes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)";
+        "CREATE TABLE IF NOT EXISTS gerenciadornomesbd (" +
+        "id SERIAL PRIMARY KEY, " +
+        "nome VARCHAR(30) NOT NULL)"; 
 
     private static final String INSERT_NOME_SQL = 
-        "INSERT INTO nomes (nome) VALUES (?)";
+        "INSERT INTO gerenciadornomesbd (nome) VALUES (?)";
 
     private static final String SELECT_NOMES_SQL = 
-        "SELECT nome FROM nomes";
+        "SELECT nome FROM gerenciadornomesbd";
 
     public GerenciadorNomesBD() {
         criarTabela();
     }
 
     private void criarTabela() {
-        try (Connection conn = DriverManager.getConnection(URL);
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
              Statement stmt = conn.createStatement()) {
             stmt.execute(CREATE_TABLE_SQL);
         } catch (SQLException e) {
@@ -30,7 +35,7 @@ public class GerenciadorNomesBD implements GerenciadorNomes {
     @Override
     public List<String> obterNomes() {
         List<String> nomes = new ArrayList<>();
-        try (Connection conn = DriverManager.getConnection(URL);
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(SELECT_NOMES_SQL)) {
 
@@ -45,7 +50,7 @@ public class GerenciadorNomesBD implements GerenciadorNomes {
 
     @Override
     public void adicionarNome(String nome) {
-        try (Connection conn = DriverManager.getConnection(URL);
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
              PreparedStatement pstmt = conn.prepareStatement(INSERT_NOME_SQL)) {
 
             pstmt.setString(1, nome);
